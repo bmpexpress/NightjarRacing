3adb18355ccbb03fcfa55c44a0b2b6b45a4dca8c
bmpexpress <bryn.phillips93@gmail.com>
2026-09-20 17:49:49 +0100

    Delete nightjar-data-analysis-0.9.0 directory

--- nightjar-data-analysis-0.9.0/src/main/java/com/nightjar/analysis/NightjarApplication.java
+++
@@ -1,8 +0,0 @@
-package com.nightjar.analysis;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class NightjarApplication {
- public static final String VERSION="0.9.0";
- public static void main(String[] args){SpringApplication.run(NightjarApplication.class,args);}
-}
